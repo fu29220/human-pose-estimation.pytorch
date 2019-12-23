e7aaa95b0d9b43c102a8d3203e08eb51a295a9fc
fushilian <fushilian@360.cn>
2019-12-23 13:39:56 +0800

    change gpus setting

--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+# gpu ids
+DEVICE_IDS=0,1,2,3
+export CUDA_VISIBLE_DEVICES=$DEVICE_IDS
+
 python pose_estimation/valid.py \
     --cfg experiments/coco/resnet50/256x192_d256x3_adam_lr1e-3.yaml \
     --flip-test \
-    --model-file models/pytorch/pose_coco/pose_resnet_50_256x192.pth.tar
+    --model-file models/pytorch/pose_coco/pose_resnet_50_256x192.pth.tar \
+    --gpus ${DEVICE_IDS}
--- train.sh
+++ train.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# gpu ids
+DEVICE_IDS=0,1,2,3
+export CUDA_VISIBLE_DEVICES=$DEVICE_IDS
+
 python pose_estimation/train.py \
-    --cfg experiments/coco/resnet50/256x192_d256x3_adam_lr1e-3.yaml
+    --cfg experiments/coco/resnet50/256x192_d256x3_adam_lr1e-3.yaml \
+    --gpus ${DEVICE_IDS}
